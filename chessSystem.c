e04cb7b4566aeb9421af4e7b5c6990c91dad3ee3
hagai2171 <hagai.g2171@gmail.com>
2021-05-18 10:02:04 +0300

    start

--- chessSystem.c
+++ chessSystem.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "chessSystem.h"
+#include "map.h"
 
-int main() {
-    printf("Hello, World!\n");
-    int hagai = 0;
-    return 0;
+typedef struct game_data_t {
+    int player_1_id;
+    int player_2_id;
+    Winner winner;
+    int duration;
+} *GameData;
+
+typedef struct game_t {
+    GameData data;
+    struct game_t *next;
+} *Game;
+
+typedef struct tournament_data_t {
+    Game head;
+    int id;
+    int venue;
+    int winner;
+} *TournamentData;
+
+typedef struct tournament_t {
+    TournamentData data;
+    struct tournament_t *next;
+} *Tournament;
+
+typedef struct chess_system_t {
+    Tournament head;
+} *ChessSystem;
+
+ChessSystem chessCreate(){
+
+
 }
